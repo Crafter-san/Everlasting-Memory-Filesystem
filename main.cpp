223e2c8985b3b0bbc7b08796a5577f6dd624f5d7
Crafter-san <74330895+Crafter-san@users.noreply.github.com>
2023-03-20 19:18:17 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include "ememfs.hpp"
+#include <type_traits>
 void main() {
 	Device device = Device();
 	Drive drive = Drive("D");
 	Folder folder = Folder("Fun Folder");
 	User user = User("Fun User");
 	File file = File("Fun file", "HOI!");
-	File& fileinstance = device.drives.Set(drive).users.Set(user).folders.Set(folder).files.Set(file);
-	fileinstance.Append("Test", true);
-	fileinstance.Append("Reeee");
-	fileinstance.metadata.Set("meta", "this be getting 'meta'-data!");
-
-	std::cout << std::endl << device.drives.Get(drive).users.Get(user).folders.Get(folder).files.Get(file).data;
-	std::cout << std::endl << device.drives.Get(drive).users.Get(user).folders.Get(folder).files.Get(file).metadata.Get("meta");
+	Drive& driveInstance = device.drives.Set(drive);
+	User& userInstance = driveInstance.users.Set(user);
+	Folder& folderInstance = userInstance.folders.Set(folder);
+	File& fileInstance = folderInstance.files.Set(file);
+	fileInstance.Append("Test", true);
+	fileInstance.Append("Reeee");
+	fileInstance.metadata.Set("meta", "this be getting 'meta'-data!");
+	File newfile("File two", "reeeee");
+	folderInstance.files.Create(newfile);
+	File newFile("File three", "raaa");
+	folderInstance.files.Create(newFile);
+	File& newFileInstance = folderInstance.files.Get(fileInstance);
+	newFileInstance.Append("reaafadasaf");
+	std::cout << fileInstance.data;
+	std::cout << fileInstance.metadata.Get("meta");
+	for (int i = 0; i < folderInstance.files.dir.size(); i++) {
+		std::cout << std::endl << folderInstance.files.dir[i] << std::endl;
+	}
+	//std::cout << std::endl << std::ifstream::read(drive) << '\n';
 }
